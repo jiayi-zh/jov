fdb56f719b8b37e60311a8a1f156328576b86619
zhengyu <zhengyu@hd123.com>
2023-06-07 14:45:06 +0800

    Initial commit

---
+++ src/main/java/pers/yahya/jov/JavaOverviewApplication.java
@@ -0,0 +1,13 @@
+package pers.yahya.jov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOverviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pers/yahya/jov/JavaOverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.yahya.jov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOverviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
